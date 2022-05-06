fd242843e84fa28c25e081ca718aceb456ee5206
Lakhdher <bennejmanoureddine03@gmail.com>
2022-05-06 15:02:41 +0100

    last

--- src/Controller/DeleteController.php
+++ src/Controller/DeleteController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Etudiant;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DeleteController extends AbstractController
 {
-    #[Route('/delete', name: 'delete')]
-    public function index(): Response
+    #[Route('/delete/{id?0}', name: 'delete')]
+    public function index(Etudiant $Etudiant = null,ManagerRegistry $doctrine): Response
     {
-        return $this->render('delete/index.html.twig', [
-            'controller_name' => 'DeleteController',
-        ]);
+        {          if (!$Etudiant) {
+            $this->addFlash("error","personne non trouvable");
+        }
+        else {
+            $manager=$doctrine->getManager();
+            $manager->remove($Etudiant);
+            $manager->flush();
+            $this->addFlash("success","deleted!");
+        
+            }
+            return $this->redirectToRoute("affiche");
+        
     }
+}
 }
